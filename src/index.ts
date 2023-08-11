40e1eff6748cdbea6b80ab32ab632a528b867926
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-11 20:05:48 +0200

    feat: use patients route in index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import diaryRouter from "./routes/diaries";
 import diagnoseRouter from "./routes/diagnoses";
+import patientRouter from "./routes/patients";
 
 const allowedOrigins = ["*"];
 const options: cors.CorsOptions = {
@@ -21,6 +22,7 @@ app.get("/api/ping", (_req, res) => {
 
 app.use("/api/diaries", diaryRouter);
 app.use("/api/diagnoses", diagnoseRouter);
+app.use("/api/patients", patientRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
