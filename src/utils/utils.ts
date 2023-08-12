3959f882d65d4bf2611eb2b8ed20e75ca8f3425a
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-13 00:58:44 +0200

    feat: parseComment and isStrings utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,17 @@
 import { NewDiaryEntry } from "../types";
 
+const isString = (text: unknown): text is string => {
+  return typeof text === "string" || text instanceof String;
+};
+
+const parseComment = (comment: unknown): string => {
+  if (!comment || !isString(comment)) {
+    throw new Error("Incorrect or missing comment");
+  }
+
+  return comment;
+};
+
 export const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
   console.log(object);
   const newEntry: NewDiaryEntry = {
